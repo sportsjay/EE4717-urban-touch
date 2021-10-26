a378b5b0c8d7e1403b561ce403d3cad022beebac
Francisco Bryant <frbryant00@gmail.com>
2021-10-26 10:16:50 +0800

    minor changes

--- app/pages/home/php/index.php
+++ app/pages/home/php/index.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" type="text/css" href="../css/index.css" />
   <title>Home</title>
+  <script src="https://kit.fontawesome.com/f8c6106aef.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
