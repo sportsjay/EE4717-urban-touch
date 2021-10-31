a2d0e88f083fcae6a6327989af91e11a3791a911
Jason Nathaniel <jasonnathaniel1152@gmail.com>
2021-10-31 16:38:32 +0800

    feature: filter based on brand and get by brand

--- app/sql/query/get_product.php
+++ app/sql/query/get_product.php
@@ -12,6 +12,12 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+/**
+ * ### Get product based on product ID
+ * 
+ * @param int $id
+ * @return string[]|string|null|false
+ */
 function getProduct($id)
 {
   global $conn;
@@ -31,6 +37,12 @@ function getProduct($id)
   mysqli_close($conn);
 }
 
+/**
+ * ### Get n-number of products 
+ * 
+ * @param int $n
+ * @return string[]|string|null|false
+ */
 function getProducts($n)
 {
   global $conn;
@@ -47,11 +59,45 @@ function getProducts($n)
   }
 }
 
+/**
+ * ### Get n-number of random products 
+ * 
+ * @param int $n
+ * @return string[]|string|null|false
+ */
 function getRandomProducts($n)
 {
   global $conn;
 
   $sql = "SELECT product.id as prod_id, product.name as prod_name, price, gender, rating, sale_status, category.name as cat_name FROM product JOIN category ON category.id=product.category_id ORDER BY RAND() LIMIT " . $n;
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_error($conn)) {
+    return mysqli_error($conn);
+  } else if (mysqli_num_rows($result) > 0) {
+    return $result;
+  } else {
+
+    return "0 results";
+  }
+
+  mysqli_close($conn);
+}
+
+/**
+ * ### Get n-number of random products by brand
+ * 
+ * @param string $n 
+ * @param string $brand
+ * @return string[]|string|null|false
+ */
+function getRandomProductByBrand($n = 3, $brand = "")
+{
+  global $conn;
+
+  $sql = "SELECT product.id as prod_id, product.name as prod_name, price, gender, rating, sale_status, category.name as cat_name FROM product JOIN category ON category.id=product.category_id";
+  if ($brand) $sql .= " WHERE brand=" . "'" . $brand . "'";
+  $sql .=  " ORDER BY RAND() LIMIT " . $n;
+
   $result = mysqli_query($conn, $sql);
   if (mysqli_error($conn)) {
     return mysqli_error($conn);
